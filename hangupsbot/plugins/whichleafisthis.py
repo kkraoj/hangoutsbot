5dd19674cbc56dcfa38fb040c7111686b4a13ecc
kkraoj <kkraoj@gmail.com>
2018-06-09 14:25:01 -0700

    trims reply to 3 preds

--- hangupsbot/plugins/whichleafisthis.py
+++ hangupsbot/plugins/whichleafisthis.py
@@ -15,7 +15,7 @@ def _initialise(bot):
 
 def tellme(bot, event, *args):
     """remember value for current user, memory must be empty.
-    use /bot forgetme to clear previous storage
+    use /bot forgestme to clear previous storage
     """
 
     text = event.text
@@ -35,9 +35,14 @@ def tellme(bot, event, *args):
                         labels[0],
                         confidences[0]))
         else:
-            for (label,confidence)  in zip(labels, confidences):
+            for idx, (label,confidence)  in enumerate(zip(labels, confidences)):
                 yield from bot.coro_send_message(
                     event.conv,
                     _("Species: <i><b>{}</b></i> \n Confidence: <b>{}</b>%").format(
                         label,
                         confidence))
+                if idx ==2:
+                    yield from bot.coro_send_message(
+                        event.conv,
+                        "Could not identify, please try again")
+                    
